ceefe94a3cefb3e995d87ee8c4a77ea8c9cb8f8c
AbulHamidi <91587368+AbulHamidi@users.noreply.github.com>
2022-04-09 17:42:21 +0300

    Update palvelut.py

--- palvelut.py
+++ palvelut.py
@@ -1,11 +1,14 @@
 class Asiakas:
   
-  """Luokan Asiakas konstruktori saa parametreina nimen ja iän. Konstruktorissa luodaan asiakasnumero käyttämällä luo_nro-metodia.
+  """
+  Luokan Asiakas konstruktori saa parametreina nimen ja iän. Konstruktorissa luodaan asiakasnumero käyttämällä luo_nro-metodia.
   nämä toiminnot laittaa asiakkaalle nimi, ikä ja numero.
   """
   
   def __init__(self, nimi, ika, numero):
-    """tässä on muuttujat:
+    
+    """
+    tässä on muuttujat:
     :ivar nimi: asiakas
     :ivar ika: asiakkaan ika
     :ivar numero: asiakasnumero joka arvotaan satunnaisesti
@@ -17,3 +20,12 @@ class Asiakas:
     self.__nimi = nimi
     self.__ika = ika
     self.__numero = numero
+
+  def luo_nro(self):
+    """
+    Konstruktorissa luodaan asiakasnumero käyttämällä luo_nro-metodia.
+    arvotaan asiakasnumero satunnaisesti
+    :ivar numero: arvottu satunnainen asiakasnumero
+    :type numero: int
+    """
+    
