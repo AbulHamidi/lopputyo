ed7955f5fed8088b8f101bd9f0e769e170de4e2f
AbulHamidi <91587368+AbulHamidi@users.noreply.github.com>
2022-04-09 17:27:55 +0300

    Update palvelut.py

--- palvelut.py
+++ palvelut.py
@@ -1,6 +1,6 @@
 class Asiakas:
   """Luokan Asiakas konstruktori saa parametreina nimen ja iän. Konstruktorissa luodaan asiakasnumero käyttämällä luo_nro-metodia."""
-  def (self, nimi, ika, ):
-    self.nimi = nimi
-    self.ika = ika
+  def __init__(self, nimi, ika):
+    self.__nimi = nimi
+    self.__ika = ika
     
